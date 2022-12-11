67b08f53072b24068f2a0df3c25840feb339513d
Tonny Oseko <tonnyoseko0@gmail.com>
2022-12-12 01:09:20 +0300

    changes: refactor typo changes on component routes in index.js file

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,10 @@ import Freelancers from "../Components/Freelancers/Freelancers";
 import Services from "../Components/Services/Services";
 import TrendingServices from "../Components/TrendingServices/TrendingServices";
 import About from "../Components/About/About";
-import Nav from "../components/Nav/Nav";
-import AppDownload from "../components/AppDownload/AppDownload";
-import Footer from "../components/Footer/Footer";
-import Hero from "../components/Herosec/Hero";
+import Nav from "../Components/Nav/Nav";
+import AppDownload from "../Components/AppDownload/AppDownload";
+import Footer from "../Components/Footer/Footer";
+import Hero from "../Components/Herosec/Hero";
 import Leads from "../Components/Leads/Leads";
 import NavBar from "../Components/Nav/NavBar";
 
